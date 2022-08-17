010b542c51df048be452b5f771d5639a8fe53f2d
pauloleroy <pauloleroy@gmail.com>
2022-08-16 21:14:42 -0300

    create a class to get question from api

---
+++ getquestions.py
@@ -0,0 +1,22 @@
+from requests import get
+
+class Questions():
+    def __init__(self, num_qs, difficulty):
+        url = f'https://opentdb.com/api.php?amount={num_qs}&difficulty={difficulty}&type=multiple'
+        response = get(url).json()
+        self.response = response['results']
+    def get_questions(self):
+        self.qs = []
+        for result in self.response:
+            self.qs.append(result['question'])
+        return self.qs
+    def get_correct_answer(self):
+        self.ans = []
+        for result in self.response:
+            self.ans.append(result['correct_answer'])
+        return self.ans
+    def get_inc_answer(self):
+        self.inc_ans = []
+        for result in self.response:
+            self.inc_ans.append(result['incorrect_answers'])
+        return self.inc_ans
